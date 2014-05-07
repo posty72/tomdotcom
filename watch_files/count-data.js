0b7d0d57e0d4b6a20b67996279c074612b6910cd
posty72 <josh.post.001@gmail.com>
2014-05-07 16:27:53 +1200

    Added watch page and updated logos

---
+++ watch_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"Join the discussion","multiple":"{num} Comments","one":"1 Comment"}},"counts":[]});
+}
